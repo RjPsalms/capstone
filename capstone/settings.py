1782f5f1a876951c862eda0739cace7cc1477834
RjPsalms <157780202+RjPsalms@users.noreply.github.com>
2024-07-24 07:51:27 +0400

    debug False, CDN bootstrap

--- capstone/settings.py
+++ capstone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5l^46d*gi*+*nh@yz&micedz&*du(d($281uq*(2kb#t3q53+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
