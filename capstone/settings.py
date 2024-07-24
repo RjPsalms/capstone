12ac36b1e508fc831a74d8794d9fe1c82a4e3acf
RjPsalms <157780202+RjPsalms@users.noreply.github.com>
2024-07-24 19:15:16 +0400

    removed the new one

--- capstone/settings.py
+++ capstone/settings.py
@@ -188,11 +188,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'website/static'),
 )
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
